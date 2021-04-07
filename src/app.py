ae10e0b431a64f464da8fc4aed613698eccd907b
Socheat Chhun <42795005+chhunsocheat@users.noreply.github.com>
2021-04-07 21:13:14 +1000

    create mongoDB database
    user can now send custom form to the backend and it will save that custom form in a mongoDB collection

--- src/app.py
+++ src/app.py
@@ -2,40 +2,80 @@ import json
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session,jsonify
 from google.auth.transport import requests
 from google.oauth2.id_token import verify_oauth2_token
 from functools import wraps
-
+import uuid
+import pymongo
 load_dotenv()  # Load dotenv before importing project level packages
 
 from drive import GoogleDrive
 from flow import get_flow, login_user
-from models import User, OneTimeURL, db
+
+# from models import User, OneTimeURL, db
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../db.sqlite"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#mongoDB
+client=pymongo.MongoClient("localhost",27017)
+db=client.portant_app
 
-db.init_app(app)
+# import routes
+#SQL
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../db.sqlite"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# db.init_app(app)
 
 
-@app.context_processor
-def inject_user():
-    return dict(user=User.get_current())
+#########################################
+# Custom form creation and save to mongoDB database
+class Form:
 
+    def createForm(self):
 
+        data = json.loads(request.data)
+        form={
+            "_id":uuid.uuid4().hex,
+            "createBy":"Socheat",
+            "formObj":data
+        }
+        db.forms.insert_one(form)
+        return jsonify(message="Success",data=data), 200
+
+@app.route('/rec-user-form',methods=['POST'])
+def rec_user_form():
+    return Form().createForm()
+
+# when user create their own form 
+@app.route("/createform")
+def createForm():
+    
+    return render_template("createform.html")
+
+############################################
+
+# @app.context_processor
+# def inject_user():
+#     return dict(user=User.get_current())
+
+#main route
 @app.route("/")
 def index():
     
+
+
+
     return render_template("index.html")
 # //in app sign in
 
 
-@app.route("/createform")
-def createForm():
-    return render_template("createform.html")
+
+
+
+
+
 
 
 @app.route("/signup")
@@ -43,31 +83,34 @@ def signup():
     return render_template("signup.html")
 
 
-def check_Valid_URL(function):
-    @wraps(function)
-    def wrapper(URL):
-        print("Checking URL sxists")
-        if (OneTimeURL.CheckURLExists(URL) == False):
-            print("URL doesn't exist")
-            redirect("/")
-        return function()
-    return wrapper
+    
 
 
-@app.route("/page/<URL>")
-@check_Valid_URL
-def respondents_Submission(URL):
-    context = {}
-    return render_template("respondents_submission_page.html", **context)
+# def check_Valid_URL(function):
+#     @wraps(function)
+#     def wrapper(URL):
+#         print("Checking URL sxists")
+#         if (OneTimeURL.CheckURLExists(URL) == False):
+#             print("URL doesn't exist")
+#             redirect("/")
+#         return function()
+#     return wrapper
+
+
+# @app.route("/page/<URL>")
+# @check_Valid_URL
+# def respondents_Submission(URL):
+#     context = {}
+#     return render_template("respondents_submission_page.html", **context)
 
 
 varible_name = "hello"
 # example of dynamic routing and capturing variable from url
 # https://dev.to/ketanip/routing-in-flask-23ff#:~:text=Dynamic%20routing%20means%20getting%20dynamic,dynamic%20input%20from%20the%20URL.&text=You%20may%20get%20data%20from,but%20is%20recommended%20to%20use.&text=It%20will%20convert%20the%20given,pass%20it%20to%20the%20function.
-@app.route('/<varible_name>/')
-def DynamicUrl(varible_name):
-    OneTimeURL.CreateURL(1)
-    return str(varible_name)
+# @app.route('/<varible_name>/')
+# def DynamicUrl(varible_name):
+#     OneTimeURL.CreateURL(1)
+#     return str(varible_name)
 
 
 @app.route("/google-api")
@@ -95,18 +138,18 @@ def oauth_callback():
     return redirect("/")
 
 
-@app.route("/make-document", methods=["POST"])
-def make_document():
-    data = json.loads(request.data)
+# @app.route("/make-document", methods=["POST"])
+# def make_document():
+#     data = json.loads(request.data)
 
-    user = User.get_current()
-    drive = GoogleDrive(user)
+#     user = User.get_current()
+#     drive = GoogleDrive(user)
 
-    document_id = drive.create_document(data["document-title"])
-    drive.append_document_text(document_id, data["document-body"])
+#     document_id = drive.create_document(data["document-title"])
+#     drive.append_document_text(document_id, data["document-body"])
 
-    payload = {"url": f"https://docs.google.com/document/d/{document_id}"}
-    return (json.dumps(payload), 200)
+#     payload = {"url": f"https://docs.google.com/document/d/{document_id}"}
+#     return (json.dumps(payload), 200)
 
 
 @app.route("/logout")
---
+++ src/mongomodel.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request, session, redirect
+from passlib.hash import pbkdf2_sha256
+from app import db
+import uuid
+import json
+
+
+class Form:
+
+    def createForm(self):
+
+        data = json.loads(request.data)
+        form={
+            "_id":uuid.uuid4().hex,
+            "createBy":"Socheat",
+            "formObj":data
+        }
+        db.forms.insert_one(form)
+        return jsonify(message="Success",data=data), 200
